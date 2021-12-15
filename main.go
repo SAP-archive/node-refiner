bd2751ac20b81df0ff09a466849898c790596c63
Ali Soliman <me@solimanali.com>
2021-12-15 04:02:04 +0100

    Handle logger synchronization error

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"log"
+
 	"github.wdf.sap.corp/ICN-ML/aicore/operators/node-harvester/pkg/controller"
 	"go.uber.org/zap"
-	"log"
 )
 
 func main() {
-	log.Printf("Application Started")
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
-	zap.ReplaceGlobals(logger)
+	log.Printf("Starting Node Refiner")
 
-	log.Printf("Starting Node Harvester")
+	logger, _ := zap.NewProduction()
+
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			log.Fatalf("Error while flushing logger: %v", err)
+		}
+	}()
+
+	zap.ReplaceGlobals(logger)
+	zap.S().Info("Setting up zap as global logger")
+
 	c, err := controller.NewController()
 	if err != nil {
-		panic(err)
+		zap.S().Fatal("Unable to instantiate the controller")
 	}
 	go c.RunCalculationLoop()
 	c.CreateRunInformers()
